ae240e89c07afd8a1b63f7df95a02efde3327c63
Martin Melo <nitram-210397@hotmail.com>
2019-02-12 16:27:25 -0600

    Layout ready

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Layout, Menu } from 'antd'
 import 'antd/dist/antd.css'
 import { NavLink, Switch, Route } from 'react-router-dom'
-import './App.css';
+import './App.scss';
 import Home from './components/home/Home'
 import Products from './components/products/Products'
 import Patients from './components/patients/Patients'
@@ -13,37 +13,42 @@ class App extends Component {
     const { Item } = Menu
     return (
       <Layout className="layout">
-        <Header>
+        <Header className="header">
           <Menu
-          theme="dark"
+          className="menu"
           mode="horizontal"
           defaultSelectedKeys={['home']}
           style={{lineHeight: "64px"}}
           >
-            <Item key="home">
-            <NavLink to="/">
+            <Item key="home" className="header-footer-text">
+            <NavLink to="/" className="items-color-white">
               Inicio
             </NavLink>
             </Item>
-            <Item key="products">
-            <NavLink to="products">
+            <Item key="products" className="header-footer-text">
+            <NavLink to="products" className="items-color-white">
             Productos
             </NavLink>
             </Item>
-            <Item key="patients">
-            <NavLink to="patients">
+            <Item key="patients" className="header-footer-text">
+            <NavLink to="patients" className="items-color-white">
             Pacientes
             </NavLink>
             </Item>
           </Menu>
         </Header>
-        <Content>
+        <Content className="content">
+          <div className="main-container">
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/products" component={Products} />
             <Route exact path="/patients" component={Patients} />
           </Switch>
+          </div>
         </Content>
+        <Footer className="footer">
+          <p className="header-footer-text">Todos los derechos reservados Actosoft 2019.</p>
+        </Footer>
       </Layout>
     );
   }
